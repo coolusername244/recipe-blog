e9d746e2d6d7cf7762efcac003ad479275252822
coolusername244 <leesheppard2404@gmail.com>
2020-12-20 19:22:26 +0000

    Added form for new recipes and tested functionality with MongoDB

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -26,6 +27,38 @@ def recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
 
+
+@app.route("/add-recipe", methods=["GET", "POST"])
+def add_recipe():
+    if request.method == "POST":
+        recipe = {
+            "name": request.form.get("name"),
+            "time_required": request.form.get("time_required"),
+            "preheat_oven": request.form.get("preheat_oven"),
+            "ingredients": request.form.get("ingredients"),
+            "allergy_info": request.form.get("allergy_info"),
+            "description": request.form.get("description"),
+            "step_one": request.form.get("step_one"),
+            "step_two": request.form.get("step_two"),
+            "step_three": request.form.get("step_three"),
+            "step_four": request.form.get("step_four"),
+            "step_five": request.form.get("step_five"),
+            "step_six": request.form.get("step_six"),
+            "step_seven": request.form.get("step_seven"),
+            "step_eight": request.form.get("step_eight"),
+            "step_nine": request.form.get("step_nine"),
+            "step_ten": request.form.get("step_ten"),
+            "step_eleven": request.form.get("step_eleven"),
+            "step_twelve": request.form.get("step_twelve"),
+            "step_thirteen": request.form.get("step_thirteen"),
+            "step_fourteen": request.form.get("step_fourteen"),
+            "step_fifteen": request.form.get("step_fifteen"),
+            "comments": request.form.get("comments")
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe Successfully Added!")
+        return redirect(url_for("recipes"))
+    return render_template("add_recipe.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
