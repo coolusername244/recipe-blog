0c499cc07a0001f12c4c2d2c863d21d45daddee3
coolusername244 <leesheppard2404@gmail.com>
2020-12-27 16:00:54 +0000

    fix bug with button not working on mobile device, take 3

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,20 @@
   let i = 5
 
-  $(document).ready(function(){
+$(document).ready(function(){
     $('.sidenav').sidenav({edge: "top"});
     $('select').formSelect();
-    $('#add_new').on('click touchstart', function() {
-        i++;
-        let html = `<div class="row step">
-                        <div class="input-field col s2 center">
-                            <strong class="">Step ${i}:</strong>
-                        </div>
-                        <div class="div col s10">
-                            <textarea id="step_${i}" name="step_${i}" minlength="5" class="materialize-textarea validate required"></textarea>
-                            <label for="step_${i}"></label>
-                        </div>
-                    </div>`
-        $('.steps').append(html);
     });
-  });
+
+document.getElementById("add_new").addEventListener("click", function() {
+    i++;
+    let html = `<div class="row step">
+                    <div class="input-field col s2 center">
+                        <strong class="">Step ${i}:</strong>
+                    </div>
+                    <div class="div col s10">
+                        <textarea id="step_${i}" name="step_${i}" minlength="5" class="materialize-textarea validate required"></textarea>
+                        <label for="step_${i}"></label>
+                    </div>
+                </div>`
+    $('.steps').append(html);
+});
