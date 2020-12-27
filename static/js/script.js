a98393ad39b751fea0f5a6e4eec1444900837091
coolusername244 <leesheppard2404@gmail.com>
2020-12-27 15:23:16 +0000

    added 'Add New Step' button on add recipe form

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,19 @@
+  let i = 5
+
   $(document).ready(function(){
     $('.sidenav').sidenav({edge: "top"});
     $('select').formSelect();
+    $(this).on("click", "#add_new", function(){
+        i++;
+        let html = `<div class="row step">
+                        <div class="input-field col s2 center">
+                            <strong class="">Step ${i}:</strong>
+                        </div>
+                        <div class="div col s10">
+                            <textarea id="step_${i}" name="step_${i}" minlength="5" class="materialize-textarea validate required"></textarea>
+                            <label for="step_${i}"></label>
+                        </div>
+                    </div>`
+        $('.steps').append(html);
+    });
   });
