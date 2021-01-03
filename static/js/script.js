0f1026c23426950544dc0b172386a82a80990d77
coolusername244 <leesheppard2404@gmail.com>
2021-01-03 11:37:33 +0000

    made recipe form responsive for mobile

--- static/js/script.js
+++ static/js/script.js
@@ -8,10 +8,10 @@ $(document).ready(function(){
 document.getElementById("add_new").addEventListener("click", function() {
     i++;
     let html = `<div class="row step">
-                    <div class="input-field col s2 center">
+                    <div class="input-field col s12 m2 center">
                         <strong class="">Step ${i}:</strong>
                     </div>
-                    <div class="div col s10">
+                    <div class="col s10 offset-s1 m10">
                         <textarea id="step_${i}" name="step[]" minlength="5" class="materialize-textarea validate required"></textarea>
                     </div>
                 </div>`
