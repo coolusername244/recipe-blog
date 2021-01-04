241e5be5d98e1959bc5998954b0229096324fd6d
coolusername244 <leesheppard2404@gmail.com>
2021-01-04 19:35:03 +0000

    added register page

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import (
     jsonify)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -69,6 +70,30 @@ def add_recipe():
         return redirect(url_for("recipes"))
     categories = mongo.db.categories.find()
     return render_template("add_recipe.html", categories=categories)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists!")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(
+                request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful!")
+        return redirect(url_for("profile", username=session["user"]))
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
