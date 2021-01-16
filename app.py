1459c58a7b87e34a529dd5cead4b02f6e2bc7ab7
coolusername244 <leesheppard2404@gmail.com>
2021-01-16 00:37:22 +0000

    Debug = False

--- app.py
+++ app.py
@@ -221,4 +221,4 @@ def edit_recipe(recipe_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
